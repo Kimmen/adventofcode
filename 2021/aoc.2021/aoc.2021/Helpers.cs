3bfea6950489f923eebcc3c19280b8317272654c
Joakim Rosendahl <joakim.rosendahl@tofindout.se>
2021-12-04 07:54:50 +0100

    Day 1

---
+++ 2021/aoc.2021/aoc.2021/Helpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Aoc;
+
+public static class Helpers
+{
+    public static IEnumerable<string> ReadLinesFromResource(string resourceName)
+    {
+        using var stream = typeof(Helpers).Assembly.GetManifestResourceStream(resourceName);
+        using var reader = new StreamReader(stream);
+
+        return reader.ReadToEnd().GetLines();
+    }
+    
+    public static IEnumerable<string> GetLines(this string value)
+    {
+        return value.Split(Environment.NewLine);
+    }
+}
