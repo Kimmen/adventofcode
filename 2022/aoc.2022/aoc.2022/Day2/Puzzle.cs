93fd93ac8db0c9dc0f0929d599630c76f134eaac
Joakim Rosendahl <1217354+Kimmen@users.noreply.github.com>
2022-12-03 09:18:53 +0100

    Day 2-2

--- 2022/aoc.2022/aoc.2022/Day2/Puzzle.cs
+++ 2022/aoc.2022/aoc.2022/Day2/Puzzle.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 using Xunit;
@@ -10,16 +11,25 @@ public class Puzzle
 {
     private readonly ITestOutputHelper _output;
 
+    private static readonly IDictionary<Shape, Shape> _winsOver = new Dictionary<Shape, Shape>
+    {
+        [Shape.Rock] = Shape.Scissor,
+        [Shape.Paper] = Shape.Rock,
+        [Shape.Scissor] = Shape.Paper,
+    };
+
+    private static readonly IDictionary<Shape, Shape> _loseTo = _winsOver.ToDictionary(x => x.Value, x => x.Key);
+
     public Puzzle(ITestOutputHelper output)
     {
         _output = output;
     }
 
-    private static long CalculateTotalScore(string inputName, int includeTopCount)
+    private static int CalculateTotalScore(string inputName, Func<Shape, string, Shape> yourShapeEvaluator)
     {
         var totalScore = Helpers.ReadLinesFromResource(inputName)
             .Select(x => x.Split(' '))
-            .Select(x => (Opponent: GetShapeFromOpponent(x[0]), Your: GetShapeFromYour(x[1])))
+            .Select(x => (Opponent: GetShapeFromOpponent(x[0]), Your: yourShapeEvaluator(GetShapeFromOpponent(x[0]), x[1])))
             .Select(x => CalculateShapeScore(x.Your) + CalculateRoundScore(x.Opponent, x.Your))
             .Sum();
 
@@ -28,16 +38,9 @@ public class Puzzle
 
     private static int CalculateRoundScore(Shape opponent, Shape your)
     {
-        return (opponent, your) switch
-        {
-            (Shape.Rock, Shape.Scissor) => 0,
-            (Shape.Paper, Shape.Rock) => 0,
-            (Shape.Scissor, Shape.Paper) => 0,
-            (Shape.Scissor, Shape.Rock) => 6,
-            (Shape.Rock, Shape.Paper) => 6,
-            (Shape.Paper, Shape.Scissor) => 6,
-            _ => 3
-        };
+        if (_winsOver[opponent] == your) return 0; 
+        if (_loseTo[opponent] == your) return 6; 
+        else return 3;
     }
 
     private static int CalculateShapeScore(Shape shape)
@@ -51,20 +54,9 @@ public class Puzzle
         };
     }
 
-    private static Shape GetShapeFromYour(string yourChoice)
+    private static Shape GetShapeFromOpponent(string opponent)
     {
-        return yourChoice switch
-        {
-            "X" => Shape.Rock,
-            "Y" => Shape.Paper,
-            "Z" => Shape.Scissor,
-            _ => throw new NotImplementedException()
-        };
-    }
-
-    private static Shape GetShapeFromOpponent(string opponentChoice)
-    {
-        return opponentChoice switch
+        return opponent switch
         {
             "A" => Shape.Rock,
             "B" => Shape.Paper,
@@ -73,18 +65,56 @@ public class Puzzle
         };
     }
 
+    private static Shape StraightYourShapeEvalutor(Shape _, string your)
+    {
+        return your switch
+        {
+            "X" => Shape.Rock,
+            "Y" => Shape.Paper,
+            "Z" => Shape.Scissor,
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    private static Shape OutcomeYourShapeEvalutor(Shape opponent, string outcome)
+    {
+        return outcome switch
+        {
+            "X" => _winsOver[opponent],
+            "Y" => opponent,
+            "Z" => _loseTo[opponent],
+            _ => throw new NotImplementedException()
+        };
+    
+    }
+
     [Fact]
     public void Part1Dev()
     {
-        var totalScore = CalculateTotalScore("Aoc.Day2.input.dev.txt", 1);
+        var totalScore = CalculateTotalScore("Aoc.Day2.input.dev.txt", StraightYourShapeEvalutor);
         Assert.StrictEqual(15, totalScore);
     }
 
     [Fact]
     public void Part1()
     {
-        var totalScore = CalculateTotalScore("Aoc.Day2.input.txt", 1);
+        var totalScore = CalculateTotalScore("Aoc.Day2.input.txt", StraightYourShapeEvalutor);
         _output.WriteLine($"Total score: {totalScore}");
         Assert.StrictEqual(12586, totalScore);
+    }
+
+    [Fact]
+    public void Part2Dev()
+    {
+        var totalScore = CalculateTotalScore("Aoc.Day2.input.dev.txt", OutcomeYourShapeEvalutor);
+        Assert.StrictEqual(12, totalScore);
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        var totalScore = CalculateTotalScore("Aoc.Day2.input.txt", OutcomeYourShapeEvalutor);
+        _output.WriteLine($"Total score: {totalScore}");
+        Assert.StrictEqual(13193, totalScore);
     }
 }
