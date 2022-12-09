9276284f21e6c7e5ac716c7ab971c64da68cd036
Joakim Rosendahl <1217354+Kimmen@users.noreply.github.com>
2022-12-09 19:53:10 +0100

    Day 9 - 2 wip

--- 2022/aoc.2022/aoc.2022/Day9/Puzzle.cs
+++ 2022/aoc.2022/aoc.2022/Day9/Puzzle.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Runtime;
+
 using Newtonsoft.Json;
 using Xunit;
 using Xunit.Abstractions;
@@ -17,80 +19,79 @@ public class Puzzle
         _output = output;
     }
     
-    private record struct Pos(int x, int y);
+    private record struct Knot(int x, int y);
 
-    private int GetTailPositionCount(string input)
+    private int GetTailPositionCount(string input, int ropeLength)
     {
         var headActions = InputReader
             .ReadLinesFromResource(input)
             .ToArray();
 
-        var tailPositions = new HashSet<Pos>(headActions.Length * 10);
-        var headPos = new Pos(0, 0);
-        var tailPos = new Pos(0, 0);
+        var visited = new HashSet<Knot>();
+        var rope = Enumerable.Repeat(new Knot(0, 0), ropeLength).ToList();
+
         foreach (var action in headActions)
         {
             var direction = action[0];
-            var steps = action[2] - '0';
+            var steps = int.Parse(action[2..]);
 
             for (var i = 0; i < steps; i++)
             {
-                headPos = Move(headPos, direction);
-                tailPos = MoveTail(tailPos, headPos, direction);
+                rope[0] = Move(rope[0], direction);
 
-                tailPositions.Add(tailPos);
+                for (int j = 1; j < rope.Count; j++)
+                {
+                    rope[j] = MoveKnot(rope[j], rope[j - 1]);
+                }
+
+                visited.Add(rope.Last());
             }
         }
 
-        return tailPositions.Count;
+        return visited.Count;
     }
 
-    private Pos Move(Pos pos, char direction, bool reverse = false)
+    private Knot Move(Knot pos, char direction)
     {
-        var reverseFactor = reverse ? -1 : 1;
         return direction switch
         {
-            'R' => pos with { x = pos.x + reverseFactor },
-            'L' => pos with { x = pos.x - reverseFactor },
-            'U' => pos with { y = pos.y + reverseFactor },
-            'D' => pos with { y = pos.y - reverseFactor },
+            'R' => pos with { x = pos.x + 1 },
+            'L' => pos with { x = pos.x - 1 },
+            'U' => pos with { y = pos.y + 1 },
+            'D' => pos with { y = pos.y - 1 },
             _ => throw new InvalidOperationException()
         };
     }
 
-    private Pos MoveTail(Pos tail, Pos head, char direction)
+    private Knot MoveKnot(Knot current, Knot ahead)
     {
-        // if (tail.y > head.y + 1)
-        // {
-        //     tail = tail with { y = tail.y - 1 };
-        //     if(tail.x)
-        // }
-        
-        var dx = head.x - tail.x;
-        var dy = head.y - tail.y;
-        var xDist = Math.Abs(dx);
-        var yDist = Math.Abs(dy);
-        
-        if (xDist <= 1 && yDist <= 1)
+        var (dx, dy) = (ahead.x - current.x, ahead.y - current.y);
+
+        if(Math.Abs(dx) > 1 || Math.Abs(dy) > 1)
         {
-            return tail;
+            return current with
+            {
+                x = current.x += Math.Sign(dx),
+                y = current.y += Math.Sign(dy)
+            };
+
         }
-        
-        return Move(head, direction, reverse: true);
+
+        return current;
     }
 
     [Fact]
     public void Part1Dev()
     {
-        var result = GetTailPositionCount("Aoc.Day9.input.dev.txt");
+        var result = GetTailPositionCount("Aoc.Day9.input.dev.txt", 2);
         Assert.Equal(13, result);
     }
 
     [Fact]
     public void Part1()
     {
-        var result = GetTailPositionCount("Aoc.Day9.input.txt");
-        Assert.Equal(13, result);
+        var result = GetTailPositionCount("Aoc.Day9.input.txt", 2);
+        Assert.Equal(6018, result);
     }
     //
     // [Fact]
