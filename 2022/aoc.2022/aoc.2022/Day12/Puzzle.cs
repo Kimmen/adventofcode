116ec9f4d2758b61b85339e10edcdfab03b3374b
Joakim Rosendahl <1217354+Kimmen@users.noreply.github.com>
2022-12-12 22:19:07 +0100

    Day 12 - 2 refactor

--- 2022/aoc.2022/aoc.2022/Day12/Puzzle.cs
+++ 2022/aoc.2022/aoc.2022/Day12/Puzzle.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Xml.Linq;
+
 using Xunit;
 using Xunit.Abstractions;
 
@@ -16,27 +18,40 @@ public class Puzzle
         _output = output;
     }
 
-    private int DetermineShortestPath(string input)
+    private int DetermineShortestHike(string input, Func<(Terrain terrain, Elevation start), Elevation[]> determineStartingPoints)
     {
         var data = InputReader.ReadContentFromResource(input);
         var (terrain, start, end) = Terrain.Parse(data);
 
-        var shortestPath = AStar.DetermineShortestPath(terrain, start, end);
+        var startingPoints = determineStartingPoints((terrain, start));
+        var minSteps = int.MaxValue;
 
-        return shortestPath.Count() - 1;
+        foreach (var startingPoint in startingPoints)
+        {
+            var shortestPath = AStar.DetermineShortestPath(terrain, start, end);
+            minSteps = Math.Min(minSteps, shortestPath.Count() - 1);
+        }
+
+
+        return minSteps;
+    }
+
+    private Elevation[] HikeOnlyFromStart((Terrain terrain, Elevation start) x)
+    {
+        return new[] { x.start }; 
     }
 
     [Fact]
     public void Part1Dev()
     {
-        var shortestPath = DetermineShortestPath("Aoc.Day12.input.dev.txt");
+        var shortestPath = DetermineShortestHike("Aoc.Day12.input.dev.txt", HikeOnlyFromStart);
         Assert.Equal(31, shortestPath);
     }
 
     [Fact]
     public void Part1()
     {
-        var shortestPath = DetermineShortestPath("Aoc.Day12.input.txt");
+        var shortestPath = DetermineShortestHike("Aoc.Day12.input.txt", HikeOnlyFromStart);
         Assert.Equal(391, shortestPath);
     }
     //
