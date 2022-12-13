dc555eaa961ef117c96a1375a9787f7175c7886c
Joakim Rosendahl <1217354+Kimmen@users.noreply.github.com>
2022-12-13 22:14:37 +0100

    Day 13-1

--- 2022/aoc.2022/aoc.2022/Day13/Puzzle.cs
+++ 2022/aoc.2022/aoc.2022/Day13/Puzzle.cs
@@ -14,7 +14,6 @@ public class Puzzle
 
     public abstract class Packet
     {
-        
     }
     public sealed class ValuePacket : Packet
     {
@@ -23,33 +22,98 @@ public class Puzzle
     public sealed class ArrayPacket : Packet
     {
         public ArrayPacket? Parent { get; set; }
-        public List<Packet> Packets { get; set; } = new List<Packet>();
+        public Queue<Packet> Packets { get; set; } = new Queue<Packet>();
 
         public void Add(Packet packet)
         {
-            this.Packets.Add(packet);
+            this.Packets.Enqueue(packet);
+        }
+
+        internal Packet? Next()
+        {
+            if(this.Packets.TryDequeue(out var packet)) return packet;
+            return null;
         }
     }
 
     private int CalculatePackagesInRightOrder(string input)
     {
-        var packatPairs = InputReader
+        var packetPairs = InputReader
             .ReadLinesFromResource(input)
             .ChunkBy(string.IsNullOrWhiteSpace)
             .Select(ParsePackets)
+            .Select(IsInRightOrder)
+            .Select(x => x!.Value)
             .ToList();
 
-        return 0;
+        return packetPairs
+            .Select((x, i) => (Index: i + 1, IsInOrder: x))
+            .Where(x => x.IsInOrder)
+            .Sum(x => x.Index);
     }
 
-    private (Packet First, Packet Last) ParsePackets(IEnumerable<string> pairsInput)
+    private bool? IsInRightOrder((ArrayPacket First, ArrayPacket Second) pair)
+    {
+        var (first, second) = pair;        
+        var left = first.Next();
+        var right = second.Next();
+
+        while (left is not null && right is not null)
+        {
+            var leftValue = left as ValuePacket;
+            var rightValue = right as ValuePacket;
+            var leftArray = left as ArrayPacket;
+            var rightArray = right as ArrayPacket;
+            
+            if (leftValue is not null && rightValue is not null
+                && leftValue.Value != rightValue.Value)
+            {
+                return leftValue.Value < rightValue.Value;
+            }
+
+            if (leftArray is not null && rightValue is not null)
+            {
+                rightArray = new ArrayPacket();
+                rightArray.Add(rightValue);
+            } else if (rightArray is not null && leftValue is not null)
+            {
+                leftArray = new ArrayPacket();
+                leftArray.Add(leftValue);
+            }
+
+            if (leftArray is not null && rightArray is not null)
+            {
+                var rightOrder = IsInRightOrder((leftArray, rightArray));
+                if (rightOrder.HasValue)
+                {
+                    return rightOrder.Value;
+                }
+            }
+
+            left = first.Next();
+            right = second.Next();
+        }
+
+        if (left is null && right is not null)
+        {
+            return true;
+        }
+        else if (left is not null && right is null)
+        {
+            return false;
+        }
+
+        return null; //inconclusive;
+    }
+
+    private (ArrayPacket First, ArrayPacket Second) ParsePackets(IEnumerable<string> pairsInput)
     {
 
         return (ParsePacket(pairsInput.First()), ParsePacket(pairsInput.Last()));
 
     }
 
-    private Packet ParsePacket(string input)
+    private ArrayPacket ParsePacket(string input)
     {
         var i = 1;
         var currentArray = new ArrayPacket();
@@ -83,7 +147,7 @@ public class Puzzle
         return currentArray;
     }
 
-    private ValuePacket ParseValue(string input, ref int i)
+    private static ValuePacket ParseValue(string input, ref int i)
     {
         var current = input[i];
         var valueInput = new List<char>();
@@ -92,7 +156,7 @@ public class Puzzle
             valueInput.Add(current);
             current = input[++i];
         }
-        //go back a step
+        //unparse last char, let the main loop take care of it.
         i--;
 
         return new ValuePacket
@@ -105,15 +169,15 @@ public class Puzzle
     public void Part1Dev()
     {
         var result = CalculatePackagesInRightOrder("Aoc.Day13.input.dev.txt");
-        Assert.Equal(12, result);
+        Assert.Equal(13, result);
     }
 
-    //[Fact]
-    //public void Part1()
-    //{
-    //    var shortestPath = DetermineShortestHike("Aoc.Day12.input.txt", HikeOnlyFromStart);
-    //    Assert.Equal(391, shortestPath);
-    //}
+    [Fact]
+    public void Part1()
+    {
+        var result = CalculatePackagesInRightOrder("Aoc.Day13.input.txt");
+        Assert.Equal(6420, result);
+    }
 
     //[Fact]
     //public void Part2Dev()
