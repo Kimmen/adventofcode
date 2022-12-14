cf392d1be348c9454d4a5ab7ff08c415db5e5688
Joakim Rosendahl <1217354+Kimmen@users.noreply.github.com>
2022-12-14 23:18:33 +0100

    Day 14 - 1

---
+++ 2022/aoc.2022/aoc.2022/Day14/Puzzle.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+
+using Xunit;
+
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Aoc.Day14;
+
+public class Puzzle
+{
+    private int DetermineUnitOfSands(string input)
+    {
+        var addedSand = 0;
+        var (plottedRockFormation, min, max) = RockFormation.Plot(input);
+
+        (int x, int y)? sandPosition;
+        do
+        {
+            sandPosition = MoveSand((500, 0), plottedRockFormation, min, max);
+            if(sandPosition.HasValue)
+            {
+                plottedRockFormation.Add(sandPosition.Value);
+                addedSand++;
+            }
+            
+        } while (sandPosition.HasValue);
+        
+        return addedSand;
+    }
+
+    private bool InsideBounderies((int x, int y) p, (int x, int y) min, (int x, int y) max)
+    {
+        if(p.x < min.x || p.x > max.x) { return false; }
+        if(p.y < min.y || p.y > max.y) { return false; } 
+        
+        return true;
+    }
+
+    private (int x, int y)? MoveSand((int x, int y) s, HashSet<(int x, int y)> occupied, (int x, int y) min, (int x, int y) max)
+    {
+        while (InsideBounderies(s, min, max))
+        {
+            (int x, int y) next;
+            if (!occupied.Contains(next = (s.x, s.y + 1)) ||
+                !occupied.Contains(next = (s.x - 1, s.y + 1)) ||
+                !occupied.Contains(next = (s.x + 1, s.y + 1)))
+            {
+                s = next;
+                continue;
+            }
+
+            return s;
+        }
+
+        return null;
+    }
+
+    [Fact]
+    public void Part1Dev()
+    {
+        var result = DetermineUnitOfSands("Aoc.Day14.input.dev.txt");
+        Assert.Equal(24, result);
+    }
+
+    [Fact]
+    public void Part1()
+    {
+        var result = DetermineUnitOfSands("Aoc.Day14.input.txt");
+        Assert.Equal(24, result);
+    }
+
+    //[Fact]
+    //public void Part2Dev()
+    //{
+    //    var result = CalculateDecoderKey("Aoc.Day13.input.dev.txt");
+    //    Assert.Equal(140, result);
+    //}
+
+    //[Fact]
+    //public void Part2()
+    //{
+    //    var result = CalculateDecoderKey("Aoc.Day13.input.txt");
+    //    Assert.Equal(22000, result);
+    //}
+}
---
+++ 2022/aoc.2022/aoc.2022/Day14/RockFormation.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Aoc.Day14;
+
+internal partial class RockFormation
+{
+    private static readonly VertexComparer _comparer = new();
+    internal static (HashSet<(int x, int y)> Rocks, (int x, int y) Min, (int x, int y) Max) Plot(string input)
+    {
+        var plottedShapes = new HashSet<(int x, int y)>();
+        var shapes = InputReader
+            .ReadLinesFromResource(input)
+            .Select(ParseShape);
+
+        //Determine boundery
+        var minX = int.MaxValue;
+        var maxX = int.MinValue;
+        var maxY = int.MinValue;
+
+        var processVertex = ((int x, int y) v) =>
+        {
+            if(v.x < minX) minX = v.x;
+            if(v.x > maxX) maxX = v.x;
+            if(v.y > maxY) maxY = v.y;
+
+            plottedShapes.Add(v);
+        };
+
+
+        foreach (var shape in shapes)
+        {
+            PlotShape(shape, processVertex);
+        }
+
+        return (plottedShapes, (minX, 0), (maxX, maxY));
+    }
+
+    private static IEnumerable<(int x, int y)> ParseShape(string line)
+    {
+        var matches = EdgeFactory().Matches(line);
+        foreach (var match in matches.AsEnumerable())
+        {
+            yield return (int.Parse(match.Groups["x"].Value), int.Parse(match.Groups["y"].Value));
+        }
+    }
+
+    private static void PlotShape(IEnumerable<(int x, int y)> shapes, Action<(int x, int y)> process)
+    {
+        var shapesArray = shapes.ToArray();
+
+        for (int i = 1; i < shapesArray.Length; i++)
+        {
+            var curr = shapesArray[i - 1];
+            var end = shapesArray[i];
+
+            process(curr);
+            while (curr != end)
+            {
+                var dx = Math.Sign(end.x - curr.x);
+                var dy = Math.Sign(end.y - curr.y);
+                curr = (curr.x + dx, curr.y + dy);
+
+                process(curr);
+            }
+        }
+    }
+
+    [GeneratedRegex("(?'x'\\d+),(?'y'\\d+)", RegexOptions.Compiled)]
+    private static partial Regex EdgeFactory();
+}
+
+public class VertexComparer : IComparer<(int x, int y)>
+{
+    public int Compare((int x, int y) a, (int x, int y) b)
+    {
+        if (a.x < b.x)
+        {
+            return -1;
+        }
+
+        if (a.x > b.x)
+        {
+            return 1;
+        }
+
+        if (a.y < b.y)
+        {
+            return -1;
+        }
+        
+        if (a.y > b.y)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+}
