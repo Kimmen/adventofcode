70131fa6a4cc0243e68602bb26dff0ded5fef290
Joakim Rosendahl <1217354+Kimmen@users.noreply.github.com>
2022-12-15 08:24:31 +0100

    Day 14 -2

--- 2022/aoc.2022/aoc.2022/Day14/Puzzle.cs
+++ 2022/aoc.2022/aoc.2022/Day14/Puzzle.cs
@@ -1,25 +1,34 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Transactions;
 
 using Xunit;
 
-using static System.Net.Mime.MediaTypeNames;
-
 namespace Aoc.Day14;
+
+using Contains = System.Func<(int x, int y), HashSet<(int x, int y)>, (int x, int y), (int x, int y), bool>;
+using ValidPos = System.Func<(int x, int y), HashSet<(int x, int y)>, (int x, int y), (int x, int y), bool>;
 
 public class Puzzle
 {
-    private int DetermineUnitOfSands(string input)
+    private int DetermineUnitOfSands(string input, Contains contains, ValidPos validPos)
     {
         var addedSand = 0;
         var (plottedRockFormation, min, max) = RockFormation.Plot(input);
 
+        bool ContainsPredicate((int x, int y) p)
+        {
+           return contains(p, plottedRockFormation!, min, max);
+        } 
+
+        bool ValidPosPredicate((int x, int y) p)
+        {
+            return validPos(p, plottedRockFormation, min, max);
+        }
+
         (int x, int y)? sandPosition;
         do
         {
-            sandPosition = MoveSand((500, 0), plottedRockFormation, min, max);
+            sandPosition = MoveSand((500, 0), ContainsPredicate, ValidPosPredicate);
             if(sandPosition.HasValue)
             {
                 plottedRockFormation.Add(sandPosition.Value);
@@ -31,58 +40,62 @@ public class Puzzle
         return addedSand;
     }
 
-    private bool InsideBounderies((int x, int y) p, (int x, int y) min, (int x, int y) max)
+    private static (int x, int y)? MoveSand((int x, int y) s, Predicate<(int x, int y)> contains, Predicate<(int x, int y)> validPos)
     {
-        if(p.x < min.x || p.x > max.x) { return false; }
-        if(p.y < min.y || p.y > max.y) { return false; } 
-        
-        return true;
-    }
-
-    private (int x, int y)? MoveSand((int x, int y) s, HashSet<(int x, int y)> occupied, (int x, int y) min, (int x, int y) max)
-    {
-        while (InsideBounderies(s, min, max))
+        while (validPos(s))
         {
             (int x, int y) next;
-            if (!occupied.Contains(next = (s.x, s.y + 1)) ||
-                !occupied.Contains(next = (s.x - 1, s.y + 1)) ||
-                !occupied.Contains(next = (s.x + 1, s.y + 1)))
+            if (contains(next = (s.x, s.y + 1)) &&
+                contains(next = (s.x - 1, s.y + 1)) &&
+                contains(next = (s.x + 1, s.y + 1)))
             {
-                s = next;
-                continue;
+                return s;
             }
 
-            return s;
+            s = next;
         }
 
         return null;
     }
 
+    private bool InsideBounderies((int x, int y) p, (int x, int y) min, (int x, int y) max)
+    {
+        if (p.x < min.x || p.x > max.x) { return false; }
+        if (p.y < min.y || p.y > max.y) { return false; }
+
+        return true;
+    }
+
+    private bool IsPlottedOrHitRockBottom((int x, int y) p, HashSet<(int x, int y)> plotted, (int x, int y) min, (int x, int y) max)
+    {
+        return plotted.Contains(p) || p.y > max.y + 1;
+    }
+
     [Fact]
     public void Part1Dev()
     {
-        var result = DetermineUnitOfSands("Aoc.Day14.input.dev.txt");
+        var result = DetermineUnitOfSands("Aoc.Day14.input.dev.txt", (p, plotted, _,  _) => plotted.Contains(p), (p, _, min, max) => InsideBounderies(p, min, max));
         Assert.Equal(24, result);
     }
 
     [Fact]
     public void Part1()
     {
-        var result = DetermineUnitOfSands("Aoc.Day14.input.txt");
+        var result = DetermineUnitOfSands("Aoc.Day14.input.txt", (p, plotted, _, _) => plotted.Contains(p), (p, _, min, max) => InsideBounderies(p, min, max));
         Assert.Equal(793, result);
     }
 
-    //[Fact]
-    //public void Part2Dev()
-    //{
-    //    var result = CalculateDecoderKey("Aoc.Day13.input.dev.txt");
-    //    Assert.Equal(140, result);
-    //}
+    [Fact]
+    public void Part2Dev()
+    {
+        var result = DetermineUnitOfSands("Aoc.Day14.input.dev.txt", IsPlottedOrHitRockBottom, (p, plotted, _, _) => p != (500, 0) || !plotted.Contains(p));
+        Assert.Equal(93, result);
+    }
 
-    //[Fact]
-    //public void Part2()
-    //{
-    //    var result = CalculateDecoderKey("Aoc.Day13.input.txt");
-    //    Assert.Equal(22000, result);
-    //}
+    [Fact]
+    public void Part2()
+    {
+        var result = DetermineUnitOfSands("Aoc.Day14.input.txt", IsPlottedOrHitRockBottom, (p, plotted, _, _) => p != (500, 0) || !plotted.Contains(p));
+        Assert.Equal(24166, result);
+    }
 }
