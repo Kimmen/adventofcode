d29a0d5e224c590ccd123b3475ddcc65672bd306
Joakim Rosendahl <1217354+Kimmen@users.noreply.github.com>
2023-12-02 14:45:27 +0100

    wip

--- 2023/aoc.2023/src/day02/aoc-day2.ts
+++ 2023/aoc.2023/src/day02/aoc-day2.ts
@@ -8,6 +8,8 @@ import data from './input'
 import {button} from '../styles'
 
 import MyWorker from './worker?worker'
+import {Bag, Game, CubeSet, GameResult, Result, GameEvalResult, EndResult } from './worker'
+
 
 
 @customElement('aoc-day-2')
@@ -17,31 +19,58 @@ export class AocDay2 extends LitElement {
     constructor() {
         super();
     }
+    @state() games: Game[] = []
+    @state() sum: number | null = null 
 
     connectedCallback() {
         super.connectedCallback();
 
-        this.worker.onmessage = ({data}: MessageEvent<string>) => {
+        this.worker.onmessage = ({data}: MessageEvent<Result>) => {
             
+            switch(data.type) {
+                case 'game' : 
+                    const gr = data as GameResult
+                    this.games.push(gr.game)
+                    break
+                case 'eval': 
+                    const er = data as GameEvalResult
+                    const game = this.games.find(g => g.gameId == er.game.gameId)
+                    if(game) {
+                        game.impossible = er.game.impossible
+                    }
+                    break
+
+                case 'end': 
+                    const end = data as EndResult
+                    this.sum = end.sum
+                    break
+            }
             
             this.requestUpdate()
          };
     }
 
     startPart1Dev() {
-       
-        this.worker.postMessage({ input: devData })
+       this.games = []
+       this.sum = 0
+        this.worker.postMessage({ input: devData, bag: { red: 12, green: 13, blue: 14 } as Bag })
     }
 
     startPart1() {
+        this.games = []
+        this.sum = 0
         this.worker.postMessage({ input: devData })
     }
 
     startPart2Dev() {
+        this.games = []
+        this.sum = 0
         this.worker.postMessage({ input: devData })
     }
 
     startPart2() {
+        this.games = []
+        this.sum = 0
         this.worker.postMessage({ input: devData })
     }
 
@@ -54,8 +83,23 @@ export class AocDay2 extends LitElement {
             <button @click=${this.startPart2Dev} disabled>Part2.Dev</button>
             <button @click=${this.startPart2} disabled>Part2</button>
         </div>
-        <section>
-           
+        <section class=`id-sum`>
+            IdSum: ${this.sum}
+            </section>
+        <section class="games">
+            ${repeat(this.games, (g) => g.gameId, (g, index)=> html`
+                <div class=${g.impossible ? 'impossible' : ''}>
+                    <p>${index}-${g.gameId}</p>
+                        ${g.sets.map(s => html`
+                            <ul>
+                                <li class='green'>${s.green}</li>
+                                <li class='red'>${s.red}</li>
+                                <li class='blue'>${s.blue}</li>
+                            </ul>
+                        `)}
+                    </ul>
+                </div>
+            `)}
         </section>
         
         `
@@ -72,7 +116,34 @@ export class AocDay2 extends LitElement {
             gap: 1rem;
         }
 
-            /* CSS */
+        section.games div.impossible {
+            text-decoration: line-through;
+            color: rgb(64,64,64)
+        }
+
+        section.games p {
+            font-weight: bold;
+        }
+
+        section.games ul {
+            list-style: none;
+            display: inline-flex;
+            padding-left: 1rem;
+            margin-right: 1rem;
+            gap: 0.5rem;
+        }
+
+        section.games li.green {
+            color: rgb(64, 191, 96);
+        }
+
+        section.games li.blue {
+            color: rgb(57, 92, 198);
+        }
+
+        section.games li.red {
+            color: rgb(204, 51, 51);
+        }
         
         `]
 }
--- 2023/aoc.2023/src/day02/worker.ts
+++ 2023/aoc.2023/src/day02/worker.ts
@@ -1,8 +1,85 @@
-import {readLines } from '../helpers'
+import { readLines } from '../helpers'
 
-self.onmessage = (event: MessageEvent<{input: string }>) => {
-    const {input } = event.data
-    const lines = readLines(input)
+export type Bag = CubeSet
 
-   
+export type CubeSet = {
+    red: number
+    green: number
+    blue: number
+}
+
+export type Game = {
+    gameId: number
+    sets: CubeSet[]
+    impossible: boolean | unknown
+}
+
+export type Result = {
+    type: 'game' | 'eval' | 'end'
+}
+
+export type GameResult = Result & {
+    type: 'game'
+    game: Game
+}
+
+export type GameEvalResult = Result & {
+    type: 'eval'
+    game: Game
+}
+
+export type EndResult = Result & {
+    type: 'end'
+    sum: number
+}
+
+self.onmessage = (event: MessageEvent<{ input: string, bag: Bag }>) => {
+    const { input, bag } = event.data
+    const games = readLines(input).map(l => {
+        // const gameMatch = l.match(/^Game (?<id>\d+): (?<subsets>.+)$/gm)!
+        const gameMatch = /^Game (?<id>\d+): (?<subsets>.+)$/gm.exec(l)
+
+        if (!gameMatch) {
+            throw Error("Failed to match game: " + l)
+        }
+
+        const gameId = parseInt(gameMatch.groups!.id)
+        const sets = gameMatch.groups!.subsets
+            .split(';')
+            .map(set => {
+                const cubeSet: CubeSet = { red: 0, green: 0, blue: 0 }
+                set
+                    .trim()
+                    .split(',')
+                    .map(coloredCubes => {
+                        const def = coloredCubes.trim().split(' ')
+                        return { color: def[1].trim().toLowerCase(), count: parseInt(def[0].trim()) }
+                    })
+                    .forEach(x => {
+                        switch (x.color) {
+                            case 'green': cubeSet.green = x.count; break
+                            case 'blue': cubeSet.blue = x.count; break
+                            case 'red': cubeSet.red = x.count; break
+                        }
+                    })
+                
+                return cubeSet
+            })
+
+            const game: Game = { gameId, sets, impossible: null } 
+            self.postMessage({type: 'game', game  } as GameResult)
+
+            return game
+    })
+
+    const possibleGames = games.map(g => {
+        g.impossible = g.sets.some(x => x.blue > bag.blue  || x.red > bag.red || x.green > bag.green)
+        self.postMessage({type: 'eval', game: g } as GameEvalResult)
+
+        return g
+    })
+    .filter(g => !g.impossible)
+
+    const sum = possibleGames.reduce((prev, g) => prev + g.gameId , 0)
+    self.postMessage({type: 'end', sum } as EndResult)
 }
