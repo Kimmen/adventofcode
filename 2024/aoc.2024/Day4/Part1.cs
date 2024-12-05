c0b1b01c0dd1f7140c99c8e64582f173c363b42c
Joakim Rosendahl <1217354+Kimmen@users.noreply.github.com>
2024-12-05 07:13:58 +0100

    Feature/2024day4 (#4)
    * ADd part 1
    
    * Day 4 part 2
    
    ---------
    
    Co-authored-by: Joakim Rosendahl <joakim.rosendahl@tofindout.se>

--- 2024/aoc.2024/Day4/Part1.cs
+++ 2024/aoc.2024/Day4/Part1.cs
@@ -1,61 +1,112 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Immutable;
 using Spectre.Console;
 
 namespace Aoc.Day4;
 
-public partial class Part1 : IChallenge
+public class Part1 : IChallenge
 {
     private string _inputExt = "txt";
-    private long? _expectedResult = 174960292L;
+    private long? _expectedResult = 2536L;
 
     public void UseDevInput()
     {
         _inputExt = "dev.txt";
-        _expectedResult = 161;
+        _expectedResult = 18L;
     }
 
     private string PuzzleInput => $"{GetType().Namespace}.input.{_inputExt}";
 
     public void Run()
     {
-        var input = InputReader.ReadContentFromResource(PuzzleInput);
+        var lines = InputReader.StreamLines(PuzzleInput).ToImmutableArray();
+        var columnCount = lines.First().Length;
+        var rowCount = lines.Length;
+        var xmasCount = 0L;
+        var grid = new char[rowCount, columnCount];
+        var xPositions = new HashSet<(int Row, int Column)>();
 
-        var matcher = MulRegex();
-        var matches = matcher.Matches(input).ToDictionary(x => x.Index, x => x);
-
+        var xStyle = Style.Plain.Foreground(Color.Maroon).Decoration(Decoration.Bold);
+        var selectStyle = Style.Plain.Foreground(Color.Orange3).Decoration(Decoration.Bold);
         var commonStyle = Style.Plain.Foreground(Color.LightSkyBlue3_1);
-        var matchStyle = Style.Plain.Foreground(Color.DeepSkyBlue4_1).Decoration(Decoration.Bold);
 
-        var products = new List<(string Value, long Product)>();
-        // out of match range by default
-        var matchRange = Array.Empty<int>();
-        for (var i = 0; i < input.Length; i++)
+        for (var row = 0; row < lines.Length; row++)
         {
-            var character = input[i].ToString().EscapeMarkup();
-            var match = matches.GetValueOrDefault(i);
-
-            if(match is not null)
+            for (var column = 0; column < lines[row].Length; column++)
             {
-                var product = CalculateProduct(match);
-                matchRange = Enumerable.Range(match.Index, match.Length).ToArray();
-                products.Add((match.Value, product));
+                var c = lines[row][column];
+                grid[row, column] = c;
+                var isX = c == 'X';
+
+                if (isX)
+                {
+                    xPositions.Add((row, column));
+                }
+
+                var color = isX ? xStyle : commonStyle;
+
+
+                AnsiConsole.Write(new Text(c.ToString(), color));
             }
-
-            var style = matchRange.Contains(i)  ? matchStyle : commonStyle;
-            AnsiConsole.Write(new Text(character, style));
+            AnsiConsole.WriteLine();
         }
-        AnsiConsole.MarkupLine("");
 
+        Thread.Sleep(100);
 
-        AnsiConsole.MarkupLine("Product: ");
-        PrintResult(products.Sum(x => x.Product));
-    }
+        (int Rows, int Column)[] directions =
+        [
+            (0, 1), // down
+            (0, -1), // up
+            (1, 0), // right
+            (-1, 0), // left
+            (1, 1), // right down
+            (-1, -1), // left up
+            (-1, 1), // right up
+            (1, -1) // left down
+        ];
 
-    private long CalculateProduct(Match match)
-    {
-        var firstNumber = long.Parse(match.Groups[1].Value);
-        var secondNumber = long.Parse(match.Groups[2].Value);
-        return firstNumber * secondNumber;
+        var target = "XMAS".ToCharArray();
+
+        for (var i = 0; i < xPositions.Count; i++)
+        {
+
+            var matchingXmasDirections = directions
+                .Where(dir =>
+                {
+                    var (row, column) = xPositions.ElementAt(i);
+
+                    var (dr, dc) = dir;
+                    foreach (var c in target)
+                    {
+                        if (row < 0 || row >= rowCount || column < 0 || column >= columnCount)
+                        {
+                            return false;
+                        }
+
+                        var x = grid[row, column];
+                        AnsiConsole.Cursor.SetPosition(column + 1, row + 1);
+                        AnsiConsole.Write(new Text(x.ToString(), selectStyle));
+                        Thread.Sleep(100);
+                        AnsiConsole.Cursor.SetPosition(column + 1, row + 1);
+                        AnsiConsole.Write(new Text(x.ToString(), xPositions.Contains((row, column)) ? xStyle : commonStyle));
+                        if (x != c)
+                        {
+                            return false;
+                        }
+
+                        row += dr;
+                        column += dc;
+                    }
+
+                    return true;
+                })
+                .ToArray();
+            var count = matchingXmasDirections.Length;
+            xmasCount += count;
+        }
+
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupLine("XMAS count: ");
+        PrintResult(xmasCount);
     }
 
     private void PrintResult(long result)
@@ -68,7 +119,4 @@ public partial class Part1 : IChallenge
 
         AnsiConsole.MarkupLine($"[{color} bold]{result}[/]");
     }
-
-    [GeneratedRegex(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled)]
-    private static partial Regex MulRegex();
 }
---
+++ 2024/aoc.2024/Day4/Part2.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections.Immutable;
+using Spectre.Console;
+
+namespace Aoc.Day4;
+
+public class Part2 : IChallenge
+{
+    private static readonly Style CommonStyle = Style.Plain.Foreground(Color.LightSkyBlue3_1);
+    private static readonly Style AStyle = Style.Plain.Foreground(Color.Maroon).Decoration(Decoration.Bold);
+    private static readonly Style CorrectStyle = Style.Plain.Foreground(Color.Green).Decoration(Decoration.Bold);
+
+    private string _inputExt = "txt";
+    private long? _expectedResult = 1875;
+
+    public void UseDevInput()
+    {
+        _inputExt = "dev.txt";
+        _expectedResult = 9L;
+    }
+
+    private string PuzzleInput => $"{GetType().Namespace}.input.{_inputExt}";
+
+    public void Run()
+    {
+        var lines = InputReader.StreamLines(PuzzleInput).ToImmutableArray();
+        var columnCount = lines.First().Length;
+        var rowCount = lines.Length;
+
+        var xmasCount = 0L;
+        var grid = new char[rowCount, columnCount];
+        var aPositions = new HashSet<(int Row, int Column)>(); ;
+
+        for (var row = 0; row < lines.Length; row++)
+        {
+            for (var column = 0; column < lines[row].Length; column++)
+            {
+                var c = lines[row][column];
+                grid[row, column] = c;
+                var isA = c == 'A';
+
+                if (isA)
+                {
+                    aPositions.Add((row, column));
+                }
+            }
+        }
+
+        var mas = "MAS".ToCharArray();
+        var sam = "SAM".ToCharArray();
+
+        var lastPrintedRow = -1;
+        for (var i = 0; i < aPositions.Count; i++)
+        {
+            var (row, column) = aPositions.ElementAt(i);
+            if(lastPrintedRow < row)
+            {
+                lastPrintedRow = PrintSection(grid, row);
+            }
+
+            var middle = grid[row, column];
+            var topLeft = GetFromGrid(grid, row - 1, column - 1);
+            var bottomRight = GetFromGrid(grid, row + 1, column + 1);
+            var topRight = GetFromGrid(grid, row - 1, column + 1);
+            var bottomLeft = GetFromGrid(grid, row + 1, column - 1);
+
+            var matchLeftDiagonal = MatchArray(mas, topLeft, middle, bottomRight) || MatchArray(sam, topLeft, middle, bottomRight);
+            var matchRightDiagonal = MatchArray(mas, topRight, middle, bottomLeft) || MatchArray(sam, topRight, middle, bottomLeft);
+            Thread.Sleep(1);
+
+            if (matchLeftDiagonal && matchRightDiagonal)
+            {
+                MarkCurrent(column, row, middle, CorrectStyle);
+                xmasCount++;
+            }
+            else
+            {
+                MarkCurrent(column, row, middle, CommonStyle);
+            }
+        }
+
+        AnsiConsole.WriteLine();
+        AnsiConsole.MarkupLine("XMAS count: ");
+        PrintResult(xmasCount);
+    }
+
+    private const int DisplayableRowsCount = 20;
+    private static void MarkCurrent(int column, int row, char middle, Style selectStyle)
+    {
+        var cappedRow = row % DisplayableRowsCount;
+        AnsiConsole.Cursor.SetPosition(column + 1, cappedRow + 1);
+        AnsiConsole.Write(new Text(middle.ToString(), selectStyle));
+        AnsiConsole.Cursor.SetPosition(column + 1, cappedRow + 1);
+    }
+
+    private static int PrintSection(char[,] grid, int row)
+    {
+        if (row % DisplayableRowsCount != 0)
+        {
+            return row;
+        }
+
+        AnsiConsole.Console.Clear();
+        var maxRowIndex = Math.Min(row + DisplayableRowsCount, grid.GetLength(0));
+        int lastRow;
+        for (lastRow = row; lastRow < maxRowIndex; lastRow++)
+        {
+            for (var c = 0; c < grid.GetLength(1); c++)
+            {
+                var cValue = grid[lastRow, c];
+                var style = cValue == 'A' ? AStyle : CommonStyle;
+
+                AnsiConsole.Write(new Text(cValue.ToString(), style));
+            }
+            AnsiConsole.WriteLine($" {lastRow}");
+        }
+
+        AnsiConsole.WriteLine($"{row} -> {maxRowIndex} / {grid.GetLength(0)}");
+        return lastRow - 1;
+    }
+
+    private static bool MatchArray(char[] compareTo, params char[] chars)
+    {
+        return compareTo.SequenceEqual(chars);
+    }
+
+    private static char GetFromGrid(char[,] grid, int row, int column)
+    {
+        if (row < 0 || column < 0 || row >= grid.GetLength(0) || column >= grid.GetLength(1))
+        {
+            return ' ';
+        }
+
+        return grid[row, column];
+    }
+
+    private void PrintResult(long result)
+    {
+        var color = _expectedResult.HasValue ? "green" : "yellow";
+        if (_expectedResult.HasValue && result != _expectedResult)
+        {
+            color = "red";
+        }
+
+        AnsiConsole.MarkupLine($"[{color} bold]{result}[/]");
+    }
+}
